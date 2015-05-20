c1a99f67860406ee9fe1e070ab3dde974ee74167
Greg Blomquist <gblomqui@redhat.com>
2015-05-20 14:25:16 -0400

    Allow for arbitrary arguments to report runner

--- lib/github_reports/runner.rb
+++ lib/github_reports/runner.rb
@@ -3,11 +3,11 @@ require_relative 'api'
 
 module GithubReports
   module Runner
-    def self.run(erb_file)
+    def self.run(erb_file, *args)
       raise ArgumentError.new("block required") unless block_given?
 
       # caller will create the report model hash
-      yield @report_model = {}
+      yield @report_model = {}, *args
       Erubis::Eruby.new(File.read(erb_file)).result(binding)
     end
   end
